bc4f8d612cfc363f8c5170866eca2997a77475f9
Asinmi Lukman <eemandigitalconcept@gmail.com>
2023-06-13 18:31:32 +0100

    still working on email authentication

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,4 +1,6 @@
 from datetime import datetime 
+#from itsdangerous import URLSafeTimedSerializer as Serializer
+
 from itsdangerous import URLSafeTimedSerializer as Serializer
 from flaskblog import db, login_manager, app
 from flask_login import UserMixin
@@ -21,12 +23,22 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
  
 
-#password recovery
+#method to create token
 def get_reset_token(self, expires_sec=1800):
     s = Serializer(app.config['SECRET_KEY'], expires_sec)
     return s.dumps({'user_id': self.id}).decode('utf-8')
 
-
+#method to verify token
+@staticmethod#remove self as an argument
+def verify_reset_token(token):
+   # Create a Serializer object with the application's secret key.
+   s = Serializer(app.config['SECRET_KEY'])
+   try:
+        user_id = s.loads(token)['user_id']
+   except:
+        return None
+   # Return the user object if the user ID is valid.
+   return User.query.get(user_id)
 
 def __repr__(self):
     return f"User('{self.username}', {self.email}, '{self.image_file}')"
